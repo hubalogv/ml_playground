f89d2f2dfa5687bb77b52b360feab233322c10d0
hubalogv <viktor.balogh@gmail.com>
2020-12-27 09:10:37 +0100

    fix data variable naming, retrain submission generation with the whole training set.

--- mnist_skl.py
+++ mnist_skl.py
@@ -8,19 +8,19 @@ from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 
 train = pd.read_csv(r"C:\_ws\datasets\mnist\train.csv")
-X_test = pd.read_csv(r"C:\_ws\datasets\mnist\test.csv")
+test = pd.read_csv(r"C:\_ws\datasets\mnist\test.csv")
 
-Y_train = train["label"]
+Y = train["label"]
 
 # Drop 'label' column
-X_train = train.drop(labels=["label"], axis=1)
+X = train.drop(labels=["label"], axis=1)
 
 random_seed = 2
 
-X = X_train/255.0
-X_test = X_test/255.0
+X = X/255.0
+X_test = test/255.0
 
-X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.1, random_state=random_seed)
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.1, random_state=random_seed)
 assert isinstance(Y_val, pd.Series)
 
 def dense_model_0():
@@ -41,8 +41,10 @@ predictions = model.predict(X_val)
 
 print('score: ', accuracy_score(Y_val, predictions))
 
-results = model.predict(X_test)
+if 1:
+    model.fit(X, Y)
+    results = model.predict(X_test)
 
-output = pd.DataFrame({'ImageId': range(1,len(results)+1),
-                       'Label': results})
-output.to_csv('submission.csv', index=False)
+    output = pd.DataFrame({'ImageId': range(1,len(results)+1),
+                           'Label': results})
+    output.to_csv('submission.csv', index=False)
