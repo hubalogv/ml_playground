d45c1149aa20652fc0e764b7fd1281653f4c99d8
hubalogv <viktor.balogh@gmail.com>
2021-01-02 08:50:46 +0100

    Tweaking learning rate. MRSLE: 0.12061423000881609

--- house_prices_keras.py
+++ house_prices_keras.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, OrdinalEncoder
 from tensorflow.keras.models import Sequential
-from tensorflow.keras import layers, callbacks
+from tensorflow.keras import layers, callbacks, regularizers, optimizers
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error, mean_squared_log_error, max_error, median_absolute_error
@@ -73,9 +73,12 @@ for col in categoric_cols:
 #     X[col] = label_encoder.fit_transform(X[col])
 #     X_test[col] = label_encoder.transform(X_test[col])
 
-X_test = pd.get_dummies(X_test ,drop_first=True)
+X_test = pd.get_dummies(X_test, drop_first=True)
 X = pd.get_dummies(X, drop_first=True)
 
+X, X_test = X.align(X_test, join='left', axis=1)
+
+print(X_test.isnull().values.any())
 
 def build_model(input_dim):
     model = Sequential()
@@ -93,17 +96,30 @@ def build_model(input_dim):
     # model.add(Dense(25, kernel_initializer='normal', activation='relu'))
     model.add(layers.Dense(1, kernel_initializer='normal'))
     # Compile model
-    model.compile(loss='mae', optimizer='adam',  metrics=["accuracy"])
+    model.compile(loss='mae', optimizer='adam',  metrics=["mean_squared_logarithmic_error"])
     return model
 
 def build_model_2(input_dim):
     model = Sequential()
     model.add(layers.Dense(400, input_dim=input_dim))
-    model.add(layers.Dropout(0.3))
-    model.add(layers.Dense(400, activation='relu'))
+    # model.add(layers.Dropout(0.3))
+    # model.add(layers.BatchNormalization())
+    model.add(layers.Activation('relu'))
+    # model.add(layers.Dense(400, activation='relu'))
     model.add(layers.Dense(1, kernel_initializer='normal'))
     # Compile model
-    model.compile(loss='mae', optimizer='adam',  metrics=["accuracy"])
+    lr_schedule = optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=0.01,
+        decay_steps=10000,
+        decay_rate=0.8)
+    model.compile(loss='mae', optimizer=optimizers.Adam(learning_rate=lr_schedule),  metrics=["mean_squared_logarithmic_error"])
+    return model
+
+def linear(input_dim):
+    model = Sequential()
+    model.add(layers.Dense(1, input_dim=input_dim))
+    # Compile model
+    model.compile(loss='mae', optimizer='adam',  metrics=["mean_squared_logarithmic_error"])
     return model
 
 if 1:
@@ -115,6 +131,7 @@ if 1:
 
 
     model = build_model_2(X_train.shape[1])
+    # model = linear(X_train.shape[1])
 
     checkpoint_filepath = 'hp_{val_loss:.4f}.h5'
     model_checkpoint_callback = callbacks.ModelCheckpoint(
@@ -127,9 +144,11 @@ if 1:
         X_train, y_train,
         validation_data=(X_valid, y_valid),
         batch_size=50,
-        epochs=500,
+        epochs=2000,
         callbacks=[model_checkpoint_callback]
     )
+
+    model.save('house_prices.h5')
 
     # Show the learning curves
     history_df = pd.DataFrame(history.history)
@@ -137,7 +156,7 @@ if 1:
 
 
     preds = model.predict(X_valid)[:,0]
-    plt.show()
+    # plt.show()
 
     print('MAX error:', max_error(y_valid, preds))
     print('Min error:', max(y_valid - preds))
@@ -148,9 +167,10 @@ if 1:
     # sns.displot(y_valid - preds)
     # plt.show()
 
-    # preds_test = model.predict(X_test)
-    # # Save test predictions to file
-    # output = pd.DataFrame({'Id': X_test.index,
-    #                        'SalePrice': preds_test})
-    # output.to_csv('submission.csv', index=False)
+if 0:
+    preds_test = model.predict(X_test)
+    # Save test predictions to file
+    output = pd.DataFrame({'Id': X_test.index,
+                           'SalePrice': preds_test})
+    output.to_csv('submission.csv', index=False)
 
